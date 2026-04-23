27a2c9b5d19844dd2c2b9fef32973a956078c18f
Abolfazl Haghpanah <abolfazlhaghpanah77@gmail.com>
2026-04-23 18:07:20 +0330

    setup koin

--- composeApp/src/iosMain/kotlin/com/paricheh/metronome/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/paricheh/metronome/MainViewController.kt
@@ -1,5 +1,14 @@
 package com.paricheh.metronome
 
 import androidx.compose.ui.window.ComposeUIViewController
+import org.koin.core.context.startKoin
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController {
+    startKoin {
+        modules(
+            // Add New modules here
+        )
+    }
+
+    App()
+}

e5b53f1d381dade9f1d1582ae40a456f0e426b10
Abolfazl Haghpanah <abolfazlhaghpanah77@gmail.com>
2026-05-07 15:53:22 +0330

    implement note class

---
+++ composeApp/src/commonMain/kotlin/com/paricheh/metronome/utils/Note.kt
@@ -0,0 +1,27 @@
+package com.paricheh.metronome.utils
+
+import androidx.compose.runtime.Stable
+
+@Stable
+class Note(
+    val weight: Int,
+    val isAccent: Boolean = false,
+    val isDot: Boolean = false,
+) {
+    init {
+        require(weight in setOf(2, 4, 8, 16))
+    }
+
+    fun getUnitCharByUnit(): String {
+        return buildString {
+            when (weight) {
+                2 -> append(MusicSymbols.HALF_NOTE)
+                4 -> append(MusicSymbols.QUARTER_NOTE)
+                8 -> append(MusicSymbols.EIGHT_NOTE)
+                16 -> append(MusicSymbols.SIXTEENTH_NOTE)
+            }
+            if (isAccent) append(MusicSymbols.ACCENT)
+            if (isDot) append(MusicSymbols.COMBINING_AUGMENTATION_DOT)
+        }
+    }
+}

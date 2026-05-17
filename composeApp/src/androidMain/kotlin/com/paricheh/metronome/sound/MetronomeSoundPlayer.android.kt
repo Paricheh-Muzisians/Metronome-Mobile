6e42b801e133bc8877965a8384b4657037807944
Abolfazl Haghpanah <abolfazlhaghpanah77@gmail.com>
2026-05-17 21:33:02 +0330

    change tick sounds

--- composeApp/src/androidMain/kotlin/com/paricheh/metronome/sound/MetronomeSoundPlayer.android.kt
+++ composeApp/src/androidMain/kotlin/com/paricheh/metronome/sound/MetronomeSoundPlayer.android.kt
@@ -7,24 +7,27 @@ import android.content.Context
 
 actual class MetronomeSoundPlayer(context: Context) {
     private val soundPool: SoundPool = SoundPool.Builder()
-        .setMaxStreams(2)
+        .setMaxStreams(20)
         .setAudioAttributes(
             AudioAttributes.Builder()
-                .setUsage(AudioAttributes.USAGE_MEDIA)
-                .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                .setUsage(AudioAttributes.USAGE_GAME)
+                .setContentType(AudioAttributes.CONTENT_TYPE_UNKNOWN)
                 .build()
         )
         .build()
 
     private var normalTickId: Int = 0
     private var accentTickId: Int = 0
+    private var subBeatTickId: Int = 0
+
     private var isLoaded = false
 
     init {
         // Load sounds from resources
         // We'll add the audio files to res/raw/
-        normalTickId = soundPool.load(context, R.raw.tick_normal, 1)
-        accentTickId = soundPool.load(context, R.raw.tick_accent, 1)
+        normalTickId = soundPool.load(context, R.raw.click_normal, 1)
+        accentTickId = soundPool.load(context, R.raw.click_accent, 1)
+        subBeatTickId = soundPool.load(context, R.raw.bottle_accent, 1)
 
         // Wait for sounds to load
         soundPool.setOnLoadCompleteListener { _, _, status ->
@@ -53,6 +56,6 @@ actual class MetronomeSoundPlayer(context: Context) {
     actual fun playSubBeat() {
         if (!isLoaded) return
 
-        soundPool.play(normalTickId, 1.0f, 1.0f, 1, 0, 1.0f)
+        soundPool.play(subBeatTickId, 1.0f, 1.0f, 1, 0, 1.0f)
     }
 }

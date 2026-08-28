89aaf43cab68446dc1540d141c19abe98c541431
Abolfazl Haghpanah <abolfazlhaghpanah77@gmail.com>
2026-08-28 14:45:16 +0330

    remove App preview

--- androidApp/src/main/kotlin/com/paricheh/metronome/MainActivity.kt
+++ androidApp/src/main/kotlin/com/paricheh/metronome/MainActivity.kt
@@ -6,11 +6,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,10 +28,4 @@ class MainActivity : ComponentActivity() {
             App()
         }
     }
-}
-
-@Preview
-@Composable
-fun AppAndroidPreview() {
-    App()
 }
